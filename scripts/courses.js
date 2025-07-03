f779522cc74d49c077daa510ca1b0f3321eec983
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-03 10:34:40 -0600

    started credits

---
+++ scripts/courses.js
@@ -0,0 +1,86 @@
+const courses = [
+        {
+        "type": "WDD",
+        "code": 130,
+        "Name" : "Web-Fundamentals",
+        "Completed" : true
+        },
+        {
+        "type": "WDD",
+        "code": 131,
+        "Name" : "Dynamic-Web-Fundamentals",
+        "Completed" : true
+        },
+        {
+        "type": "WDD",
+        "code": 231,
+        "Name" : "Web Frontend Development",
+        "Completed" : false
+        },
+        {
+        "type": "CSE",
+        "code": 111,
+        "Name" : "Programming with Functions",
+        "Completed" : true
+        },
+        {
+        "type": "CSE",
+        "code": 210,
+        "Name" : "Programming with Classes",
+        "Completed" : true
+        },
+        {
+        "type": "CSE",
+        "code": 110,
+        "Name" : "Introduction to Programming",
+        "Completed" : true
+        }
+    ]
+   
+const coursesContainer = document.querySelector('.courses');
+const allButton = document.querySelector(".all-button");
+const cseButton = document.querySelector(".cse-button");
+const wddButton = document.querySelector(".wdd-button");
+const creditsMessage = document.querySelector("credits-indicator")
+let credits;
+
+function getCourses(coursesArray) {
+    coursesArray.forEach(course => {
+        const courseBox = document.createElement("div");
+        courseBox.textContent = `${course.type}-${course.code}`;
+        coursesContainer.appendChild(courseBox)
+    });
+}
+
+function showCredits() {
+    creditsMessage.textContent = `The numer of credits listed`;
+}
+
+function clearCourses() {
+    coursesContainer.innerHTML = "";
+}
+
+function getAllCourses(coursesArray) {
+    clearCourses();
+    getCourses(coursesArray);
+}
+
+function GetCSECourses(coursesArray) {
+    clearCourses();
+    let newArray = coursesArray.filter(courses => courses.type === "CSE");
+    getCourses(newArray);
+}
+
+function GetWDDCourses(coursesArray) {
+    clearCourses();
+    let newArray = coursesArray.filter(courses => courses.type === "WDD");
+    getCourses(newArray);
+}
+
+allButton.addEventListener("click", () => getAllCourses(courses));
+cseButton.addEventListener("click", () => GetCSECourses(courses));
+wddButton.addEventListener("click", () => 
+GetWDDCourses(courses));
+document.addEventListener("DOMContentLoaded", () =>{
+    getAllCourses(courses)
+})
