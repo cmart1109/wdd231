cf71656abe82f16ddf58e6330a1f569cf51b7696
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-10 11:19:46 -0600

    starting with the assignment

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('.business-container');
+const data = "./data/members.json";
+
+async function fetchData() {
+    try {
+        const response = await fetch(data);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const members = await response.json();
+        displayMembers(members.members);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+function displayMembers(members) {
+    members.forEach(member => {
+        const card = document.createElement('section');
+        card.classList.add('card');
+        const name = document.createElement('h2');
+        const address = document.createElement('p');
+        const image = document.createElement('img');
+        const phone = document.createElement('p');
+        name.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+        image.setAttribute('src', member.image);
+        image.setAttribute('alt', `Image of ${member.name}`);
+        image.setAttribute('loading', 'lazy');
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        container.appendChild(card);
+    });
+}
+
+fetchData();
