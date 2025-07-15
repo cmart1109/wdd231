dfd3a39e78622853d4607661c91cf19b654fa98b
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-15 15:15:22 -0600

    weather

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,5 +1,5 @@
-const weatherIcon = document.querySelector('.weather-icon');
-const currentTemp = document.querySelector('.current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const currentTemp = document.querySelector('#current-temp');
 const captionDesc = document.querySelector('figcaption');
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75051&lon=6.63672&units=metric&appid=514fcabd81719b73521e0fd6cff2f824";
@@ -11,14 +11,22 @@ async function apiFetch(){
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
-        // displayResults(data)
+        displayResults(data)
       } else {
         throw Error(await response.text())
       }  
     } catch (error) {
         console.log(error);
     }
+}
+
+function displayResults(data) { 
+    currentTemp.innerHTML = `${data.main.temp} &deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;    
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc); 
+    captionDesc.textContent = `${desc}`;
 }
 
 apiFetch();
