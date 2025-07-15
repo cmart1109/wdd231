ff48be2953c365ceac7299224877ecd6d0f3e6b0
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-14 20:49:09 -0600

    exercise

---
+++ scripts/course.mjs
@@ -0,0 +1,34 @@
+const byuiCourse = {
+  code: "WDD231",
+  name: "Web Frontend Development I",
+  sections: [
+    {
+      sectionNumber: 1,
+      enrolled: 88,
+      instructor: "Brother Bingham",
+    },
+    {
+      sectionNumber: 2,
+      enrolled: 81,
+      instructor: "Sister Shultz",
+    },
+    {
+      sectionNumber: 3,
+      enrolled: 95,
+      instructor: "Sister Smith",
+    },
+  ],
+  changeEnrollment: function (sectionNumber, add = true) {
+      (section) => section.sectionNumber == sectionNumber
+    if (sectionIndex >= 0) {
+      if (add) {
+        this.sections[sectionIndex].enrolled++;
+      } else {
+        this.sections[sectionIndex].enrolled--;
+      }
+      renderSections(this.sections);
+    }
+  },
+};
+
+export default byuiCourse;
