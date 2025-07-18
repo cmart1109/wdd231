191eaee236025a0fadf6cc510190ed409e928715
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-18 11:44:19 -0600

    change strong

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -23,6 +23,7 @@ function displayMembers(members) {
         const image = document.createElement('img');
         const phone = document.createElement('p');
         const url = document.createElement('a');
+        const membership = document.createElement('p')
         name.textContent = member.name;
         address.textContent = member.address;
         phone.textContent = member.phone;
@@ -31,12 +32,15 @@ function displayMembers(members) {
         image.setAttribute('loading', 'lazy');
         url.setAttribute('href', members.url);
         url.textContent = "Website";
+        membership.textContent = `Membership Level: ${member.membership}`
         card.appendChild(image);
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
         card.appendChild(url);
+        card.appendChild(membership)
         container.appendChild(card);
+
     });
 }
 
