e2f2b719159974fd0b8c988eb925ab06b306f0c5
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-18 11:32:27 -0600

    spotlights

---
+++ chamber/scripts/spotlights.js
@@ -0,0 +1,43 @@
+const container = document.querySelector('.spotlights');
+const data = "./data/members.json";
+
+async function fetchData() {
+    try {
+        const response = await fetch(data);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const members = await response.json();
+        displayMembers(members.members) 
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+function displayMembers(members) {
+    members.forEach(member => {
+        const card = document.createElement('div');
+        card.classList.add('spotlight-card');
+        const name = document.createElement('h3');
+        const address = document.createElement('p');
+        const image = document.createElement('img');
+        const phone = document.createElement('p');
+        const url = document.createElement('a');
+        name.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+        image.setAttribute('src', member.image);
+        image.setAttribute('alt', `Image of ${member.name}`);
+        image.setAttribute('loading', 'lazy');
+        url.setAttribute('href', members.url);
+        url.textContent = "Website";
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+        container.appendChild(card);
+    });
+}
+
+fetchData();
