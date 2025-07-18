a19070f2b30e974e8f372cfcad5ee8f099227fcf
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-18 15:03:09 -0600

    filter

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -8,14 +8,17 @@ async function fetchData() {
             throw new Error('Network response was not ok');
         }
         const members = await response.json();
-        displayMembers(members.members) 
+        const membersArray = members.members;
+        const filteredArray = membersArray.filter(members => members.membership === "gold" || members.membership === "silver");
+        displayMembers(filteredArray) 
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
 }
 
 function displayMembers(members) {
-    members.forEach(member => {
+     for (let membersCount = 0; membersCount = 2; membersCount++) {
+        const randomMember = Math.floor(Math.random() * members.length);
         const card = document.createElement('div');
         card.classList.add('spotlight-card');
         const name = document.createElement('h3');
@@ -24,24 +27,25 @@ function displayMembers(members) {
         const phone = document.createElement('p');
         const url = document.createElement('a');
         const membership = document.createElement('p')
-        name.textContent = member.name;
-        address.textContent = member.address;
-        phone.textContent = member.phone;
-        image.setAttribute('src', member.image);
-        image.setAttribute('alt', `Image of ${member.name}`);
+        name.textContent = members[randomMember].name;
+        address.textContent = members[randomMember].address;
+        phone.textContent = members[randomMember].phone;
+        image.setAttribute('src', members[randomMember].image);
+        image.setAttribute('alt', `Image of ${members[randomMember].name}`);
         image.setAttribute('loading', 'lazy');
-        url.setAttribute('href', members.url);
+        url.setAttribute('href', members[randomMember].url);
         url.textContent = "Website";
-        membership.textContent = `Membership Level: ${member.membership}`
+        membership.textContent = `Membership Level: ${members[randomMember].membership}`
         card.appendChild(image);
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
         card.appendChild(url);
         card.appendChild(membership)
-        container.appendChild(card);
+        container.appendChild(card);        
+     }
 
-    });
-}
+    };
+
 
 fetchData();
