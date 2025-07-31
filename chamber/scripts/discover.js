0653f5d9df1c603fe7a1645621846756c17b43aa
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-07-31 10:40:50 -0600

    starting dicover

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,53 @@
+const gridContainer = document.querySelector('.grid-container');
+const dataUrl = "../data/locations.json";
+
+async function fetchData(data) {
+    try {
+        const response = await fetch(data);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const locations = await response.json();
+        displayLocations(locations);
+
+    } catch (error) {
+        
+        console.error('There has been a problem with your fetch operation:', error);
+
+    }
+}
+
+
+function displayLocations(locationsArray) {
+    gridContainer.innerHTML = ''; 
+    locationsArray.forEach(location => {
+        const card = document.createElement('section');
+        card.classList.add('discover-card');
+        const title = document.createElement('h2');
+        const imageContainer = document.createElement('figure');
+        const image = document.createElement('img');
+        const caption = document.createElement('figcaption');
+        const address = document.createElement('p');
+        const description = document.createElement('p');
+        const button = document.createElement('button');
+        title.textContent = location.name;
+        image.setAttribute('src', location.image);
+        image.setAttribute('alt', location.name);
+        image.setAttribute('loading', 'lazy');
+        caption.textContent = location.name;
+        imageContainer.appendChild(image);
+        imageContainer.appendChild(caption);
+        address.textContent = location.address;
+        description.textContent = location.description;
+        button.textContent = 'Learn More';
+
+        card.appendChild(title);
+        card.appendChild(imageContainer);
+        card.appendChild(address);
+        card.appendChild(description);
+        card.appendChild(button);
+        gridContainer.appendChild(card);
+    });
+}
+
+fetchData(dataUrl);
